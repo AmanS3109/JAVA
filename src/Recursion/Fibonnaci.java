d3909bd58d2a41977509e04288b0185cb412c080
AmanS3109 <singhaman2122@ternaengg.ac.in>
2024-01-05 14:49:37 +0530

    commit message

--- src/Recursion/Fibonnaci.java
+++ src/Recursion/Fibonnaci.java
@@ -1,13 +1,21 @@
 package Recursion;
 
 public class Fibonnaci {
-    public static void main(String[] args) {
-        System.out.println(fibo(7));
+    public static void main(String[] args){
+    //  for (int i = 0; i < 11; i++) {
+//            System.out.println(fibFormula(i));
+//        }
+
+        System.out.println(fibFormula(50));
     }
-    static int fibo(int n){
-        if (n < 2){
-            return n;
-        }
-        return fibo(n - 1) + fibo(n - 2);
+
+    static int fibFormula(int n){
+        return (int) ((Math.pow(((1+Math.sqrt(5))/2),n)-Math.pow(((1-Math.sqrt(5))/2),n))/Math.sqrt(5));
     }
+//    static int fibo(int n){
+//        if (n < 2){
+//            return n;
+//        }
+//        return fibo(n - 1) + fibo(n - 2);
+//    }
 }
