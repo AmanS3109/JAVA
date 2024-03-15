fe1d6003fb19a5de3d4aa74f2f9fa2536731110c
AmanS3109 <singhaman2122@ternaengg.ac.in>
2024-03-16 00:07:43 +0530

    commit message

---
+++ src/Recursion/Question2.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class Question2 {
+    public static void main(String[] args) {
+        funRev(5);
+    }
+    static void funRev(int n){
+        if (n == 0) {
+            return;
+        }
+        funRev(n-1);
+        System.out.println(n);
+
+    }
+}
