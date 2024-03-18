58dd3a636be3e5d02825f07e87afa45a7de78417
AmanS3109 <singhaman2122@ternaengg.ac.in>
2024-03-18 19:52:22 +0530

    commit message

--- src/Recursion/sortedArray.java
+++ src/Recursion/sortedArray.java
@@ -1,5 +1,16 @@
 package Recursion;
 
 public class sortedArray {
-
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 2, 6, 5};
+        System.out.println(sort(arr, 0));
+    }
+    static boolean sort(int[] arr, int index){
+        // base condition
+        if (index == arr.length - 1){
+            return true;
+        }
+        //recursive relation
+        return arr[index] < arr[index + 1] && sort(arr, index + 1);
+    }
 }
