e5974f8b47b1645351fdfe58b2a18e94f0e294dc
AmanS3109 <singhaman2122@ternaengg.ac.in>
2024-06-10 14:56:12 +0530

    commited message

---
+++ src/Hashing/Hash_Map.java
@@ -0,0 +1,15 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class Hash_Map {
+    public static void main(String[] args) {
+//        String name = "Aman";
+//        String name1 = "Shruti";
+//        Integer a = 1813;
+//        int code = name.hashCode();
+//        int hash = code % 10;
+//        System.out.println(hash);
+        HashMap<Integer, String> hashMap = new HashMap<>();
+    }
+}
