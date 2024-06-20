2aeeb2f23ddbbe96799f6aa2f987a31c3eb5d9d6
AmanS3109 <singhaman2122@ternaengg.ac.in>
2024-06-21 00:08:06 +0530

    commit message

--- src/boolTwoSum.java
+++ src/boolTwoSum.java
@@ -4,17 +4,33 @@ public class boolTwoSum {
         int target = 13;
         System.out.println(isPairSum(arr, target));
     }
+//    static boolean isPairSum(int[] arr, int target){
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = i+1; j < arr.length; j++) {
+//                if (arr[i] + arr[j] == target){
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;  // Time Complexity : O(N^2)
+// }
+    // better approach --> Two Pointers
     static boolean isPairSum(int[] arr, int target){
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[i] + arr[j] == target){
-                    return true;
-                }
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right){
+            int current_sum = arr[left] + arr[right];
+            if (current_sum == target){
+                return true;
+            } else if (current_sum < target) {
+                left++;
+            }else {
+                right--;
             }
         }
-        return false;  // Time Complexity : O(N^2)
+        return false; // Time Complexity = O(N)
     }
 }
 
-// better approach --> Two Pointers
+
 
