53b61ab5f4ed3769bdaba109dfb3c6d5fcfb705b
AmanS3109 <singhaman2122@ternaengg.ac.in>
2024-10-24 23:49:43 +0530

    commited message

---
+++ src/Trees/binaryTree.java
@@ -0,0 +1,19 @@
+package Trees;
+
+public class binaryTree {
+    static class Node{
+        int val;
+        Node left;
+        Node right;
+
+        Node(int val){
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+    }
+}
